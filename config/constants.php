96d4f9235cbdfc9667d27fb9f0cb0078e420af12
Havochalo <officialmail85@gmail.com>
2022-10-09 11:08:10 +1100

    finalsied source code

--- config/constants.php
+++ config/constants.php
@@ -3,12 +3,13 @@
 session_start();
 
 // Create Constants to store non repeating values
-define('SITEURL', 'http://localhost:81/chicken-shop/');
-define('LOCALHOST', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'chicken-shop');
+define('SITEURL', 'menaicharcoal.42web.io');
+define('LOCALHOST', 'sql305.epizy.com');
+define('DB_USERNAME', 'epiz_32749741');
+define('DB_PASSWORD', 'cO492LYo6Y4S');
+define('DB_NAME', 'epiz_32749741_chicken_shop');
 
 // Execute Query and save data in database
 $conn = mysqli_connect(LOCALHOST, DB_USERNAME, DB_PASSWORD); // Database connection
 $db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error($conn));
+    
